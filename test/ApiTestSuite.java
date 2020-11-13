8b8979579092ec5647193c74217d5f22a6a30f52
afrika <kin.afro@gmail.com>
2020-11-13 23:56:48 +0200

    finished task

---
+++ test/ApiTestSuite.java
@@ -0,0 +1,5 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestDogApi.class, TestPetStoreSwaggerApi.class})
+public class ApiTestSuite{}
